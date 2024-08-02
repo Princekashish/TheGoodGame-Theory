1332a591fae18fd0fc34b34a1823b248a526a6f1
Prince <princekashishbbu@gmail.com>
2024-08-03 01:40:03 +0530

    change

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,10 @@
 import Quotes from './components/Quotes'
-import Saved from './components/Saved'
 
 function App() {
   return (
     <>
      <div className=' text-white '>
      <Quotes/>
-     <Saved/>
      </div>
     </>
   )
